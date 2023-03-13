49d1e9508f4e8f57470871d055087c3fa85d925d
Steffe Koerner <steffen.koerner@mytum.de>
2023-03-13 18:18:09 +0100

    Remove empty lines in tests

--- bisect/test/test.cpp
+++ bisect/test/test.cpp
@@ -2,13 +2,11 @@
 #include "application.h"
 
 TEST(Sum, AdditionOfPositiveValues) {
-
   const int result = sum(3,4);
   EXPECT_EQ(result, 7);
 }
 
 TEST(Sum, AdditionOfNegativeValues) {
-
   const int result = sum(-1,-5);
   EXPECT_EQ(result, -6);
 }
