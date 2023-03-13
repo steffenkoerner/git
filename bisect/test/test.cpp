ec4703baf1d21ebdf3b2b286b0a98481114d944a
Steffe Koerner <steffen.koerner@mytum.de>
2023-03-13 18:17:41 +0100

    Add test for product

--- bisect/test/test.cpp
+++ bisect/test/test.cpp
@@ -12,3 +12,13 @@ TEST(Sum, AdditionOfNegativeValues) {
   const int result = sum(-1,-5);
   EXPECT_EQ(result, -6);
 }
+
+TEST(Product, ProductOfNegativeValues) {
+  const int result = product(-10,-15);
+  EXPECT_EQ(result, 150);
+}
+
+TEST(Product, ProductOfPositiveValues) {
+  const int result = product(3,12);
+  EXPECT_EQ(result, 36);
+}
