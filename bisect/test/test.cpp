e47b85105e288dff419344903a4e547163705efc
Steffe Koerner <steffen.koerner@mytum.de>
2023-03-13 18:13:27 +0100

    Add another test

--- bisect/test/test.cpp
+++ bisect/test/test.cpp
@@ -6,3 +6,9 @@ TEST(Sum, AdditionOfPositiveValues) {
   const int result = sum(3,4);
   EXPECT_EQ(result, 7);
 }
+
+TEST(Sum, AdditionOfNegativeValues) {
+
+  const int result = sum(-1,-5);
+  EXPECT_EQ(result, -6);
+}
