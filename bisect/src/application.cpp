6b25ee6a2aae28390849cd71e8e482167100defd
Steffe Koerner <steffen.koerner@mytum.de>
2023-03-13 18:14:31 +0100

    Add product function

--- bisect/src/application.cpp
+++ bisect/src/application.cpp
@@ -4,3 +4,7 @@ int sum(int a, int b) {
 
     return a + b;
 }
+
+int product(int a, int b) {
+    return a * b;
+}
--- bisect/src/application.h
+++ bisect/src/application.h
@@ -1,3 +1,4 @@
 # pragma once
 
 int sum(int a, int b);
+int product(int a, int b);
